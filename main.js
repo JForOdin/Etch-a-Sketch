0820d682331a884212bed1937662630fc8f29696
JForOdin <jovianbrown@protonmail.com>
2023-02-18 16:44:20 +0800

    board complete, most functionality done. working on changing resolution

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 
-const changeLargeBoard = () => {
-    console.log("Changing to large resolution");
-}
-const changeMediumBoard = () => {
-    console.log("Changing to medium resolution");
-}
 
-var paintOn = false;
-const tileWidth = 16;
-const tileHeight = 16;
-const screenWidth = 576;
-const screenHeight = 768;
-const pixels = [];
-const tilesX = screenWidth/tileWidth;  //
-const tilesY = screenHeight/tileHeight; //24
+var paintOn = true;
+var tileWidth = 16;
+var tileHeight = 16;
+const smallBoardPixelSize = "8";
+const mediumBoardPixelSize = "12";
+const largeBoardPixelSize = "16";
+const screenWidth = 576; //36
+const screenHeight = 768; //48
+var pixels = [];
+var tilesX = screenWidth/tileWidth;  //
+var tilesY = screenHeight/tileHeight; //24
 const totalTiles = tilesX*tilesY;
 const boardDiv = document.querySelector('#board-div');
 const rowDiv = document.createElement('div');
@@ -28,15 +25,37 @@ const eraseBoard = () => {
     }
 }
 const changeSmallBoard = () => {
-    console.log("Changing to small resolution");
-    eraseBoard();
-    for(let pixel of pixels)
+    createBoard(smallBoardPixelSize,smallBoardPixelSize);
+   for(let pixel of pixels)
     {
         pixel.style.height = "8px";
         pixel.style.width = "8px";
 
+    } 
+}
+const changeMediumBoard = () => {
+    console.log("Changing to small resolution");
+
+    createBoard(mediumBoardPixelSize,mediumBoardPixelSize);
+    for(pixel of pixels)
+    {
+        pixel.style.width="12px";
+        pixel.style.height="12px";
     }
 }
+const changeLargeBoard = () => {
+    console.log("Changing to Large resolution");
+  /*  eraseBoard();
+    for(let pixel of pixels)
+    {
+        pixel.style.height = "16px";
+        pixel.style.width = "16px";
+        
+    }*/
+    
+    createBoard(16,16);
+}
+
 const paint = () => {
     if(paintOn)
         paintOn=false;
@@ -44,8 +63,23 @@ const paint = () => {
         paintOn=true;
 }
 ///create board below
-const createBoard = () => 
-{
+const createBoard = (tileWidth,tileHeight) => 
+{   
+    //need to clear all the children from the boardDiv
+    var boardDivChildren = document.getElementById("board-div");
+    let child = boardDivChildren.firstElementChild;
+    while(child)
+    {
+        boardDiv.removeChild(child);
+        child = boardDivChildren.firstElementChild;
+    }
+    ////////
+
+    ///Calculate number of tiles in X axis and Y axis
+    tilesX = screenWidth/tileWidth;
+    tilesY = screenHeight/tileHeight;
+    console.log("Tiles X: "+tilesX);
+    console.log("Tile Y: "+tilesY);
     for(let i = 0; i < tilesY; i++)
     {
         const row = document.createElement('div');
@@ -56,14 +90,27 @@ const createBoard = () =>
             column.className="column-div";
            // column.innerHTML = `<img src="." width="${tileWidth}" height="${tileHeight}"></img>`;
             column.style.width=tileWidth;
-            column.style.border="1px solid dimgrey";
+            column.style.height=tileHeight;
+            //column.style.height=tileHeight;
+
+         //   column.style.border="1px solid dimgrey";
+            column.style.outline = "1px solid dimgrey";
             row.appendChild(column);
             pixels.push(column);
         }
     }
+    /// used to decide when a pixel should change color
+    pixels.forEach(element => {
+        element.addEventListener('mouseover', () => {
+           // element.style.backgroundColor = "dimgrey";
+           paintPixel(element);
+           
+        });
+    });
 }
+
 const paintPixel = (element) => {
-    if(paintOn)
+    if(paintOn) 
         element.style.backgroundColor = "dimgrey";
   /*  if(element.clicked==true)
        {
@@ -74,12 +121,6 @@ const paintPixel = (element) => {
     element.clicked = true; */
 
 }
-createBoard();
+createBoard(largeBoardPixelSize,largeBoardPixelSize);
 
-pixels.forEach(element => {
-    element.addEventListener('mouseover', () => {
-       // element.style.backgroundColor = "dimgrey";
-       paintPixel(element);
-       
-    });
-});
+
