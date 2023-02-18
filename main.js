a9f5d6a2f4342cd344a29e2d6e7016f9bf653ffd
JForOdin <jovianbrown@protonmail.com>
2023-02-18 17:26:12 +0800

    added more comments to project

--- main.js
+++ main.js
@@ -1,27 +1,11 @@
-
-
-var paintOn = true; //user can toggle whether to draw or not by clicking mouse
-var tileWidth = 16;  //initial resolution for pixels
-var tileHeight = 16;
-const smallBoardPixelSize = "8";  
-const mediumBoardPixelSize = "12";
-const largeBoardPixelSize = "16";
-const screenWidth = 576; //determines how many tiles in the Y direction
-const screenHeight = 768; //helps to determing how many tiles are in the X
-var pixels = []; //an array of  pixels
-var tilesX = screenWidth/tileWidth;  
-var tilesY = screenHeight/tileHeight; 
-const boardDiv = document.querySelector('#board-div'); //board div from index.html
-const rowDiv = document.createElement('div'); //create a div for
-rowDiv.className="row-div";
-boardDiv.appendChild(rowDiv);
-
+/// erase board sets each pixel back to neutral lightgrey color 
 const eraseBoard = () => {
     for(let pixel of pixels)
     {
         pixel.style.backgroundColor = "lightgrey"
     }
 }
+/// Below are three functions that create varying board sizes
 const changeSmallBoard = () => {
     createBoard(smallBoardPixelSize,smallBoardPixelSize);
    for(let pixel of pixels)
@@ -44,7 +28,9 @@ const changeLargeBoard = () => {
     console.log("Changing to Large resolution");
     createBoard(16,16);
 }
-
+/////////////
+/// Paint function determines the current state of the mouse click
+/// if paintOn is false dont change pixel color
 const paint = () => {
     if(paintOn)
         paintOn=false;
@@ -69,6 +55,7 @@ const createBoard = (tileWidth,tileHeight) =>
     tilesY = screenHeight/tileHeight;
     console.log("Tiles X: "+tilesX);
     console.log("Tile Y: "+tilesY);
+    /// for the number of tiles in X and Y create a new div element
     for(let i = 0; i < tilesY; i++)
     {
         const row = document.createElement('div');
@@ -77,8 +64,6 @@ const createBoard = (tileWidth,tileHeight) =>
         {
             const column = document.createElement('div');
             column.className="column-div";
-            column.style.width=tileWidth;
-            column.style.height=tileHeight;
             column.style.outline = "0.5px solid dimgrey";
             row.appendChild(column);
             pixels.push(column);
@@ -106,6 +91,22 @@ const paintPixel = (element) => {
     element.clicked = true; */
 
 }
+var paintOn = true; //user can toggle whether to draw or not by clicking mouse
+var tileWidth = 16;  //initial resolution for pixels
+var tileHeight = 16;
+const smallBoardPixelSize = "8";  
+const mediumBoardPixelSize = "12";
+const largeBoardPixelSize = "16";
+const screenWidth = 576; //determines how many tiles in the Y direction
+const screenHeight = 768; //helps to determing how many tiles are in the X
+var pixels = []; //an array of  pixels
+var tilesX = screenWidth/tileWidth;  
+var tilesY = screenHeight/tileHeight; 
+const boardDiv = document.querySelector('#board-div'); //board div from index.html
+const rowDiv = document.createElement('div'); //create a div for
+rowDiv.className="row-div";
+boardDiv.appendChild(rowDiv); //add our row div to our static html board div
+
 createBoard(largeBoardPixelSize,largeBoardPixelSize);
 
 
